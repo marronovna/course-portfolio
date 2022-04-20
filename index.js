7aea957690820725e441e62618cdf1a13a9856ab
marronovna <mariawrrr14@gmail.com>
2022-04-21 00:46:56 +0300

    1st commit

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+//DOM
 //document.querySelector(".selector") - повертає перший елемент який відповідає селектору переданому в аргумент
 //document.querySelectorAll(".selector") - повертає масив елементів які відповідає селектору переданому в аргумент
 //(будьякий елемент).innerHtml - міняє html елемента
 //(будьякий елемент).append - додає до елемента
 //document.createElement("div") - повертає новостворений елемент типу який переданий в якості парамету
+document.getElementById("tt").innerHTML="Summer evening";
+document.getElementById("mTh").innerHTML="'Summer evening' by Edvard Hopper"
+
+
+
 
